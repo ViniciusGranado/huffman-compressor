08f87449999380ac05fac65346bdae01ed04434f
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-11-28 17:31:53 -0300

    Fix GenericArray class

--- GenericArray.java
+++ GenericArray.java
@@ -2,11 +2,11 @@ import java.util.Arrays;
 
 public class GenericArray<T> {
   private final Object[] array;
-  public final int len;
+  public int len;
 
   public GenericArray(int len) {
     array = new Object [len];
-    this.len = len;
+    this.len = 0;
   }
 
   T getArr(int index)
@@ -18,6 +18,29 @@ public class GenericArray<T> {
   void setArr(int index, T object)
   {
     array[index] = object;
+  }
+
+  void add(int index, T object) {
+    T aux1 = (T) array[index];
+    T aux2 = null;
+
+    array[index] = object;
+
+    for (int i = index + 1; i <= len; i++) {
+      aux2 = (T) array[i];
+      array[i] = aux1;
+      aux1 = aux2;
+    }
+
+    len++;
+  }
+
+  T get(int index) {
+    return (T) array[index];
+  }
+
+  int size() {
+    return len;
   }
 
   @Override
